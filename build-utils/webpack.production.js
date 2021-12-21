7bec32c953c46a95d38add6334fe6cee73ca7bc9
Stanley Thijssen <stanley.thijssen@gmail.com>
2021-12-21 17:16:25 +0800

    feat: add post css configuration for production environment

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -9,7 +9,11 @@ module.exports = () => {
             rules: [
                 {
                     test: /\.css$/,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                    ],
                 },
             ],
         },
