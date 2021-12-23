46bdce72ea8788693ecd6c12055043902075c85b
Stanley Thijssen <stanley.thijssen@gmail.com>
2021-12-24 02:18:39 +0800

    feat: add css color variables for project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from 'react-dom'
 import { App } from './App'
-import './styles/variables.css'
 import './styles/normalize.css'
+import './styles/variables.css'
 
 render(<App />, document.getElementById('root'))
