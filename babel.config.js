c53a581fc10f0fe0fb9dedbdf59c4f16f5789db4
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-01-21 10:39:01 +0800

    chore: added core-js polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     presets: [
-        ['@babel/preset-env', { targets: 'defaults', modules: false }],
+        [
+            '@babel/preset-env',
+            { targets: 'defaults', modules: false, useBuiltIns: 'usage' },
+        ],
         ['@babel/preset-react', { runtime: 'automatic' }],
         ['@babel/preset-typescript'],
     ],
