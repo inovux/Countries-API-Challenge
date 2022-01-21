a91a7ccfe9ca51c1f46f3a95cbbb0c2e75bcba0f
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-01-21 23:33:56 +0800

    feat: added types for countries

---
+++ src/types/countries/index.ts
@@ -0,0 +1,23 @@
+export interface ICurrency {
+    code: string
+    name: string
+    symbol: string
+}
+
+export interface ILanguage {
+    iso639_1: string
+    iso639_2: string
+    name: string
+    nativeName: string
+}
+
+export interface ICountry {
+    capital: string
+    currencies: ICurrency[]
+    languages: ILanguage[]
+    nativeLanguage: string
+    population: number
+    region: string
+    subregion: string
+    topLevelDomain: string[]
+}
