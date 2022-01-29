17fea55e31943b3b60b6b155138666e78f68ddea
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-01-29 15:03:03 +0800

    feat: finish up Header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,11 @@ import styles from './Header.module.css'
 
 export const Header = () => {
     return (
-        <header className={styles.container}>
-            <h1>Where in the world?</h1>
-            <div>Theme switcher</div>
+        <header className={styles.headerContainer}>
+            <div className={styles.contentContainer}>
+                <h1>Where in the world?</h1>
+                <div>Theme switcher</div>
+            </div>
         </header>
     )
 }
