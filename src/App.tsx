a86b59b3f177b7c4e842809c159d8e569ae49c5c
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-01-30 19:54:58 +0800

    feat: added dummy countries to countries page

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,6 @@
 // import { countryActions } from './store/countries/actions'
 import { CountriesPage } from './pages'
 
-// const countries = [
-//     {
-//         country: 'The Netherlands',
-//         population: 20000000,
-//         region: 'Utrecht',
-//         capital: 'Amsterdam',
-//         flagUrl: 'https://flagcdn.com/w320/nl.png',
-//     },
-//     {
-//         country: 'Germany',
-//         population: 100000000,
-//         region: 'Munich',
-//         capital: 'Munich',
-//         flagUrl: 'https://flagcdn.com/w320/de.png',
-//     },
-//     {
-//         country: 'Belgium',
-//         population: 100000000,
-//         region: 'Brussels',
-//         capital: 'Brussels',
-//         flagUrl: 'https://flagcdn.com/w320/be.png',
-//     },
-//     {
-//         country: 'France',
-//         population: 100000000,
-//         region: 'Paris',
-//         capital: 'Paris',
-//         flagUrl: 'https://flagcdn.com/w320/fr.png',
-//     },
-// ]
-
 export const App = () => {
     // const dispatch = useDispatch()
     //
--- src/pages/Countries/index.tsx
+++ src/pages/Countries/index.tsx
@@ -1,11 +1,43 @@
 import { FC } from 'react'
 // import styles from './Countries.module.css'
 import { PageTemplate } from '../PageTemplate'
+import { CountryCardList } from '../../components/CountryCardList'
+
+const countries = [
+    {
+        country: 'The Netherlands',
+        population: 20000000,
+        region: 'Utrecht',
+        capital: 'Amsterdam',
+        flagUrl: 'https://flagcdn.com/w320/nl.png',
+    },
+    {
+        country: 'Germany',
+        population: 100000000,
+        region: 'Munich',
+        capital: 'Munich',
+        flagUrl: 'https://flagcdn.com/w320/de.png',
+    },
+    {
+        country: 'Belgium',
+        population: 100000000,
+        region: 'Brussels',
+        capital: 'Brussels',
+        flagUrl: 'https://flagcdn.com/w320/be.png',
+    },
+    {
+        country: 'France',
+        population: 100000000,
+        region: 'Paris',
+        capital: 'Paris',
+        flagUrl: 'https://flagcdn.com/w320/fr.png',
+    },
+]
 
 export const CountriesPage: FC = () => {
     return (
         <PageTemplate>
-            <div>Countries</div>
+            <CountryCardList countries={countries} />
         </PageTemplate>
     )
 }
