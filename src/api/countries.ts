338ff28e1f6223317582cb504d044d94865092f0
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-02-13 19:38:42 +0800

    feat: added saga for handling filtering request

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -1,6 +1,7 @@
 import { apiInstance } from './index'
+import { ICountry } from '../types'
 
 export const countriesRequests = {
-    getCountries: (params?: Record<string, any>) =>
+    getCountries: (params?: Record<string, any>): Promise<ICountry[]> =>
         apiInstance.get('/all', { params }),
 }
--- src/store/countries/saga.ts
+++ src/store/countries/saga.ts
@@ -1,14 +1,33 @@
-import { all, debounce, takeEvery, select } from 'redux-saga/effects'
-import { AnyAction } from 'redux'
+import {
+    all,
+    debounce,
+    takeEvery,
+    select,
+    SimpleEffect,
+} from 'redux-saga/effects'
 import { countryActions } from './actions'
 import { countrySelectors } from './selectors'
+import { countriesRequests } from '../../api/countries'
 
-function* filterRequest(action: AnyAction) {
+// TODO: Fix types
+function* filterRequest(): Generator<
+    Promise<any> | SimpleEffect<string>,
+    string,
+    any
+> {
     const state: ReturnType<typeof countrySelectors.filter> = yield select(
         countrySelectors.filter
     )
-    console.log(state)
-    yield action
+
+    const params = {
+        ...state,
+    }
+
+    const data = yield countriesRequests.getCountries(params)
+
+    console.log(data)
+
+    return 'Call another action with given data'
 }
 
 export default function* rootSaga() {
