079767bef6ec0b650a645054a1fa840c29780f79
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-02-17 10:37:51 +0800

    feat: added type declaration file to fix missing types in fa-svg-core

---
+++ fontawesome-svg-core.d.ts
@@ -0,0 +1,5 @@
+declare module '@fortawesome/fontawesome-svg-core/import.macro' {
+    import { IconDefinition, IconName } from '@fortawesome/fontawesome-svg-core'
+
+    function regular(iconName: IconName): IconDefinition
+}
