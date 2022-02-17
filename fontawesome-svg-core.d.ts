feaa4f3a9a61d6c964d1d8f713814dcb6a6d3042
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-02-17 10:54:15 +0800

    feat: added additional types for fontawesome

--- fontawesome-svg-core.d.ts
+++ fontawesome-svg-core.d.ts
@@ -1,5 +1,10 @@
 declare module '@fortawesome/fontawesome-svg-core/import.macro' {
     import { IconDefinition, IconName } from '@fortawesome/fontawesome-svg-core'
 
+    function brands(iconName: IconName): IconDefinition
+    function duotone(iconName: IconName): IconDefinition
+    function light(iconName: IconName): IconDefinition
     function regular(iconName: IconName): IconDefinition
+    function solid(iconName: IconName): IconDefinition
+    function thin(iconName: IconName): IconDefinition
 }
