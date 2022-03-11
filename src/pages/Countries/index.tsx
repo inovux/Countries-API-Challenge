4485e1adb9b5c4f8ceedb45af940616deb03793a
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-03-11 16:13:11 +0800

    feat: added example for countries using the api

--- src/pages/Countries/index.tsx
+++ src/pages/Countries/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { PageTemplate } from '../PageTemplate'
 import { CountryCardList } from '../../components/CountryCardList'
@@ -8,71 +8,74 @@ import { IOption } from '../../components/Select/Option'
 import { countriesRequests } from '../../api/countries'
 import { countrySelectors } from '../../store/countries/selectors'
 
-const countries = [
-    {
-        country: 'The Netherlands',
-        population: 20000000,
-        region: 'Utrecht',
-        capital: 'Amsterdam',
-        flagUrl: 'https://flagcdn.com/w320/nl.png',
-    },
-    {
-        country: 'Germany',
-        population: 100000000,
-        region: 'Munich',
-        capital: 'Munich',
-        flagUrl: 'https://flagcdn.com/w320/de.png',
-    },
-    {
-        country: 'Belgium',
-        population: 100000000,
-        region: 'Brussels',
-        capital: 'Brussels',
-        flagUrl: 'https://flagcdn.com/w320/be.png',
-    },
-    {
-        country: 'France',
-        population: 100000000,
-        region: 'Paris',
-        capital: 'Paris',
-        flagUrl: 'https://flagcdn.com/w320/fr.png',
-    },
-    {
-        country: 'France',
-        population: 100000000,
-        region: 'Paris',
-        capital: 'Paris',
-        flagUrl: 'https://flagcdn.com/w320/fr.png',
-    },
-    {
-        country: 'Belgium',
-        population: 100000000,
-        region: 'Brussels',
-        capital: 'Brussels',
-        flagUrl: 'https://flagcdn.com/w320/be.png',
-    },
-    {
-        country: 'Germany',
-        population: 100000000,
-        region: 'Munich',
-        capital: 'Munich',
-        flagUrl: 'https://flagcdn.com/w320/de.png',
-    },
-    {
-        country: 'The Netherlands',
-        population: 20000000,
-        region: 'Utrecht',
-        capital: 'Amsterdam',
-        flagUrl: 'https://flagcdn.com/w320/nl.png',
-    },
-]
+// const countries = [
+//     {
+//         country: 'The Netherlands',
+//         population: 20000000,
+//         region: 'Utrecht',
+//         capital: 'Amsterdam',
+//         flagUrl: 'https://flagcdn.com/w320/nl.png',
+//     },
+//     {
+//         country: 'Germany',
+//         population: 100000000,
+//         region: 'Munich',
+//         capital: 'Munich',
+//         flagUrl: 'https://flagcdn.com/w320/de.png',
+//     },
+//     {
+//         country: 'Belgium',
+//         population: 100000000,
+//         region: 'Brussels',
+//         capital: 'Brussels',
+//         flagUrl: 'https://flagcdn.com/w320/be.png',
+//     },
+//     {
+//         country: 'France',
+//         population: 100000000,
+//         region: 'Paris',
+//         capital: 'Paris',
+//         flagUrl: 'https://flagcdn.com/w320/fr.png',
+//     },
+//     {
+//         country: 'France',
+//         population: 100000000,
+//         region: 'Paris',
+//         capital: 'Paris',
+//         flagUrl: 'https://flagcdn.com/w320/fr.png',
+//     },
+//     {
+//         country: 'Belgium',
+//         population: 100000000,
+//         region: 'Brussels',
+//         capital: 'Brussels',
+//         flagUrl: 'https://flagcdn.com/w320/be.png',
+//     },
+//     {
+//         country: 'Germany',
+//         population: 100000000,
+//         region: 'Munich',
+//         capital: 'Munich',
+//         flagUrl: 'https://flagcdn.com/w320/de.png',
+//     },
+//     {
+//         country: 'The Netherlands',
+//         population: 20000000,
+//         region: 'Utrecht',
+//         capital: 'Amsterdam',
+//         flagUrl: 'https://flagcdn.com/w320/nl.png',
+//     },
+// ]
 
 export const CountriesPage: FC = () => {
     const dispatch = useDispatch()
     const filter = useSelector(countrySelectors.filter)
+    const [countries, setCountries] = useState([])
 
     useEffect(() => {
-        countriesRequests.getCountries({ yoyo: 15 })
+        countriesRequests.getCountries().then((data) => {
+            setCountries(data as any)
+        })
     }, [])
 
     const handleSearch = (value: string) => {
