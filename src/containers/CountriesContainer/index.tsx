6304c6950f00b2b1f778f9a1d74ad1e4ccc3bb2d
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-03-17 19:00:38 +0800

    fix: issue where countries were not show because data was not loaded yet

--- src/containers/CountriesContainer/index.tsx
+++ src/containers/CountriesContainer/index.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { IRootState } from '../../store/state'
 import { countryActions } from '../../store/countries/actions'
+import { CountryCardList } from '../../components/CountryCardList'
 
 export const CountriesContainer: FC = () => {
     const dispatch = useDispatch()
@@ -10,13 +11,12 @@ export const CountriesContainer: FC = () => {
     )
 
     useEffect(() => {
-        console.log('wtf')
         dispatch(countryActions.get.started({}))
     }, [])
 
-    if (isLoading) {
+    if (isLoading || !data.length) {
         return <div>Loading...</div>
     }
-    return <div>lul</div>
-    // return <CountryCardList countries={data} />
+
+    return <CountryCardList countries={data} />
 }
