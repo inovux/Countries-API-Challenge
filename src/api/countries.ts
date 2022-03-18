79c2909b260cc189f55022408a580caccabffac2
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-03-18 09:50:24 +0800

    feat: added getCountriesByRegion api request

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -2,10 +2,12 @@ import { apiInstance } from './index'
 import { IApiCountryView } from '../types'
 
 export const countriesRequests = {
-    getCountries: async (
-        params?: Record<string, any>
-    ): Promise<IApiCountryView> => {
-        const response = await apiInstance.get('/all', { params })
+    getCountries: async (): Promise<IApiCountryView> => {
+        const response = await apiInstance.get('/all')
+        return response.data
+    },
+    getCountriesByRegion: async (params: { region: string }) => {
+        const response = await apiInstance.get(`/continent/${params.region}`)
         return response.data
     },
 }
