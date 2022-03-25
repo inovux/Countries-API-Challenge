80b9361d5c71474f5834ee18940e7c4dd8ffa622
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-03-25 17:01:26 +0800

    feat: remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-// import { useDispatch } from 'react-redux'
-// import { CountryCardList } from './components/CountryCardList'
-// import { IOption, Option } from './components/Select/Option'
-// import { Select } from './components/Select'
-// import { countryActions } from './store/countries/actions'
 import { CountriesPage } from './pages'
 
 export const App = () => {
-    // const dispatch = useDispatch()
-    //
-    // const handleSelect = (option: IOption) => {
-    //     dispatch(countryActions.selectRegion(option))
-    // }
-
     return <CountriesPage />
 }
