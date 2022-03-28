3e57dd3976c36e8f7e026112f8c19d38420d8cb1
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-03-28 23:46:05 +0800

    feat: added basic dummy structure for the country details page

--- src/pages/Country/index.tsx
+++ src/pages/Country/index.tsx
@@ -5,5 +5,28 @@ import { PageTemplate } from '../PageTemplate'
 export const CountryPage: FC = () => {
     const { code } = useParams()
 
-    return <PageTemplate>{code}</PageTemplate>
+    return (
+        <PageTemplate>
+            <div>Back button</div>
+            <div>
+                <div>Flag</div>
+                <div>
+                    <div>
+                        <div>{code}</div>
+                        <div>Native Name: </div>
+                        <div>Population: </div>
+                        <div>Region: </div>
+                        <div>Sub Region: </div>
+                        <div>Capital: </div>
+                    </div>
+                    <div>
+                        <div>Top Level Domain: {code}</div>
+                        <div>Currencies:</div>
+                        <div>Language: {code}</div>
+                    </div>
+                </div>
+                <div>Border Countries: 1, 2, 3</div>
+            </div>
+        </PageTemplate>
+    )
 }
