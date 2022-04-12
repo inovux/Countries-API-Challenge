545e5660d1ef98fac3ada320dce7356c136bf985
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-04-12 19:33:28 +0800

    refactor: use padding on container of back button instead

--- src/pages/Country/index.tsx
+++ src/pages/Country/index.tsx
@@ -15,27 +15,27 @@ export const CountryPage: FC = () => {
 
     return (
         <PageTemplate>
-            <div className={styles.container}>
+            <div className={styles.backButton}>
                 <Button onClick={navigateBack} text="Back" />
+            </div>
+            <div>
+                <div>Flag</div>
                 <div>
-                    <div>Flag</div>
                     <div>
-                        <div>
-                            <div>{code}</div>
-                            <div>Native Name: </div>
-                            <div>Population: </div>
-                            <div>Region: </div>
-                            <div>Sub Region: </div>
-                            <div>Capital: </div>
-                        </div>
-                        <div>
-                            <div>Top Level Domain: {code}</div>
-                            <div>Currencies:</div>
-                            <div>Language: {code}</div>
-                        </div>
+                        <div>{code}</div>
+                        <div>Native Name: </div>
+                        <div>Population: </div>
+                        <div>Region: </div>
+                        <div>Sub Region: </div>
+                        <div>Capital: </div>
                     </div>
-                    <div>Border Countries: 1, 2, 3</div>
+                    <div>
+                        <div>Top Level Domain: {code}</div>
+                        <div>Currencies:</div>
+                        <div>Language: {code}</div>
+                    </div>
                 </div>
+                <div>Border Countries: 1, 2, 3</div>
             </div>
         </PageTemplate>
     )
