6ac3a9dcc6fa7120ccfe3aecd1a9a12b9e74e476
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-04-12 19:31:43 +0800

    feat: added basic padding for the CountryPage component

--- src/pages/Country/index.tsx
+++ src/pages/Country/index.tsx
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { PageTemplate } from '../PageTemplate'
 import { Button } from '../../components/Button'
 
+import styles from './Country.module.css'
+
 export const CountryPage: FC = () => {
     const { code } = useParams()
     const navigate = useNavigate()
@@ -13,25 +15,27 @@ export const CountryPage: FC = () => {
 
     return (
         <PageTemplate>
-            <Button onClick={navigateBack} text="Back" />
-            <div>
-                <div>Flag</div>
+            <div className={styles.container}>
+                <Button onClick={navigateBack} text="Back" />
                 <div>
+                    <div>Flag</div>
                     <div>
-                        <div>{code}</div>
-                        <div>Native Name: </div>
-                        <div>Population: </div>
-                        <div>Region: </div>
-                        <div>Sub Region: </div>
-                        <div>Capital: </div>
+                        <div>
+                            <div>{code}</div>
+                            <div>Native Name: </div>
+                            <div>Population: </div>
+                            <div>Region: </div>
+                            <div>Sub Region: </div>
+                            <div>Capital: </div>
+                        </div>
+                        <div>
+                            <div>Top Level Domain: {code}</div>
+                            <div>Currencies:</div>
+                            <div>Language: {code}</div>
+                        </div>
                     </div>
-                    <div>
-                        <div>Top Level Domain: {code}</div>
-                        <div>Currencies:</div>
-                        <div>Language: {code}</div>
-                    </div>
+                    <div>Border Countries: 1, 2, 3</div>
                 </div>
-                <div>Border Countries: 1, 2, 3</div>
             </div>
         </PageTemplate>
     )
