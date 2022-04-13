feca12b46a89e7f0d4801cd6a5bf80e5f1fae360
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-04-13 18:28:24 +0800

    feat: added basic style lay-out for Country page

--- src/pages/Country/index.tsx
+++ src/pages/Country/index.tsx
@@ -18,24 +18,26 @@ export const CountryPage: FC = () => {
             <div className={styles.backButton}>
                 <Button onClick={navigateBack} text="Back" />
             </div>
-            <div>
-                <div>Flag</div>
-                <div>
-                    <div>
-                        <div>{code}</div>
-                        <div>Native Name: </div>
-                        <div>Population: </div>
-                        <div>Region: </div>
-                        <div>Sub Region: </div>
-                        <div>Capital: </div>
+            <div className={styles.container}>
+                <div className={styles.flagContainer}>Flag</div>
+                <div className={styles.contentContainer}>
+                    <div className={styles.contentInnerContainer}>
+                        <div>
+                            <div>{code}</div>
+                            <div>Native Name: </div>
+                            <div>Population: </div>
+                            <div>Region: </div>
+                            <div>Sub Region: </div>
+                            <div>Capital: </div>
+                        </div>
+                        <div>
+                            <div>Top Level Domain: {code}</div>
+                            <div>Currencies:</div>
+                            <div>Language: {code}</div>
+                        </div>
                     </div>
-                    <div>
-                        <div>Top Level Domain: {code}</div>
-                        <div>Currencies:</div>
-                        <div>Language: {code}</div>
-                    </div>
+                    <div>Border Countries: 1, 2, 3</div>
                 </div>
-                <div>Border Countries: 1, 2, 3</div>
             </div>
         </PageTemplate>
     )
